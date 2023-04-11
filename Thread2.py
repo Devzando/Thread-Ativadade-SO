a154bd83244e82fafb9c3f31daaa3edd54fc76a3
Devzando <74877494+Devzando@users.noreply.github.com>
2023-04-11 03:52:10 -0300

    Add files via upload

---
+++ Thread2.py
@@ -0,0 +1,65 @@
+import threading
+import datetime
+import multiprocessing
+
+
+run_count = int(input("Digite a quantidade de vezes que cada thread deve rodar: "))
+
+def programa_1():
+    def write_to_file(file, thread_name, run_number, program_name):
+        now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        message = f"{run_number} {program_name} {thread_name} {now}\n"
+        with open(file, "a") as f:
+            f.write(message)
+
+    def thread_function(file1, file2, run_count, program_name):
+        thread_name = threading.current_thread().name
+        for i in range(run_count):
+            write_to_file(file1, thread_name, i+1, program_name)
+            write_to_file(file2, thread_name, i+1, program_name)
+
+    
+    thread1 = threading.Thread(target=thread_function, args=(
+        "registro1.txt", "registro2.txt", run_count, "programa1"), name="Thread1")
+    thread2 = threading.Thread(target=thread_function, args=(
+        "registro1.txt", "registro2.txt", run_count, "programa1"), name="Thread2")
+    thread3 = threading.Thread(target=thread_function, args=(
+        "registro1.txt", "registro2.txt", run_count, "programa1"), name="Thread3")
+    
+    thread1.start()
+    thread2.start()
+    thread3.start()
+
+
+def programa_2():
+    def write_to_file(file, thread_name, run_number, program_name):
+        now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        message = f"{run_number} {program_name} {thread_name} {now}\n"
+        with open(file, "a") as f:
+            f.write(message)
+
+    def thread_function(file1, file2, run_count, program_name):
+        thread_name = threading.current_thread().name
+        for i in range(run_count):
+            write_to_file(file1, thread_name, i+1, program_name)
+            write_to_file(file2, thread_name, i+1, program_name)
+            
+    thread1 = threading.Thread(target=thread_function, args=(
+        "registro1.txt", "registro2.txt", run_count, "programa1"), name="Thread1")
+    thread2 = threading.Thread(target=thread_function, args=(
+        "registro1.txt", "registro2.txt", run_count, "programa1"), name="Thread2")
+    thread3 = threading.Thread(target=thread_function, args=(
+        "registro1.txt", "registro2.txt", run_count, "programa1"), name="Thread3")
+    
+    thread1.start()
+    thread2.start()
+    thread3.start()
+
+
+if __name__ == '__main__':
+    p1 = multiprocessing.Process(target=programa_1)
+    p2 = multiprocessing.Process(target=programa_2)
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
